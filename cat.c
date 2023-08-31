6f48031957b339a1ef6fbb00df5b5cf059589034
EthanRule <ethan2002@live.com>
2023-08-31 08:11:56 -0700

    Base cat.c file

--- cat.c
+++ cat.c
@@ -1,1 +1,31 @@
-#include <stdio.h>void filecopy(FILE*, FILE*);int main(int argc, char *argv[]){FILE *fp;if(argc == 1) // No args; copy standard inputfilecopy(stdin, stdout);elsewhile(--argc > 0)if ((fp = fopen(*++argv, "r")) == NULL){printf("cat: can't open %s\n", *argv);return 1;}else{filecopy(fp, stdout);fclose(fp);}return 0;}void filecopy(FILE *ifp, FILE *ofp){int c;while((c = getc(ifp)) !- EOF)outc(c, ofp);}
+#include <stdio.h>
+
+void filecopy(FILE*, FILE*);
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+
+	if (argc == 1) // No args; copy standard input
+		filecopy(stdin, stdout);
+	else
+		while (--argc > 0)
+			if ((fp = fopen(*++argv, "r")) == NULL)
+			{
+				printf("cat: can't open %s\n", *argv);
+				return 1;
+			}
+			else
+			{
+				filecopy(fp, stdout);
+				fclose(fp);
+			}
+	return 0;
+}
+
+void filecopy(FILE *ifp, FILE *ofp)
+{
+	intc;
+	while ((c = getc(ifp)) != EOF)
+		putc(c, ofp);
+}
