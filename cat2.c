5c74216a76f59294d37dd83010601b220281f034
EthanRule <ethan2002@live.com>
2023-08-31 08:48:39 -0700

    cat2 tutorial stuff

--- cat2.c
+++ cat2.c
@@ -6,11 +6,40 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/*
+ * open("file name", operation) operation can be O_RDONLY | O_WRONLY | O_RDWR...
+ * write(file descripter(int), message string, sizeof the message string)
+ * lseek(file descripter(int), 0, SEEK_SET)
+ * read(file descripter(int), buffer(this is where the message will be stored), sizeof buffer);
+ * close(file descripter(int))
+ *
+ *
+ * */
+
+
+
 static void filecopy(int ifd, int ofd);
 static void error(char *fmt, ...); // can be used to generate error message
 
 int main(int argc, char *argv[]) {
 	// complete your code
+	
+	int fd;
+	char buffer[80];
+	char msg[50] = "Hello!";
+
+	fd = open("in1.txt", O_RDWR)
+	printf("fd = %d", fd);
+
+	if (fd != -1) // check to see if the file is valid/opened
+	{
+		printf("in1.txt opened for read write access\n");
+		write(fd, msg, sizeof(msg));
+		lseek(fd, 0, SEEK_SET);
+		read(fd, buffer, sizeof(msg));
+		printf("\n %s was written to my file\n", buffer);
+		close(fd);
+	}
 	return 0;
 }
 
