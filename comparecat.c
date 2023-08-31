fd3e58e05c7763fa2acfeb0891e3fcddc49f9cc6
EthanRule <ethan2002@live.com>
2023-08-31 14:54:32 -0700

    compare cat is goated, got it working

--- comparecat.c
+++ comparecat.c
@@ -18,7 +18,29 @@ void filecopy(FILE*, FILE*);
 static void filecopycathw(int ifd, int ofd);
 static void error(char *fmt, ...);
 
-main_cathw(int argc, char *argv[]) {
+int main_cat(int argc, char*argv[]) {
+        FILE *fp;
+
+        if (argc == 1) // No args; copy standard input
+                filecopy(stdin, stdout);
+        else
+                while (--argc > 0)
+                        if ((fp = fopen(*++argv, "r")) == NULL)
+                        {
+                                printf("cat: can't open %s\n", *argv);
+                                return 1;
+                        }
+                        else
+                        {
+                                filecopy(fp, stdout);
+                                fclose(fp);
+                        }
+        return 0;
+}
+
+
+
+int main_cathw(int argc, char *argv[]) {
  	// your implementation for cathw
         int fd;
 
@@ -38,42 +60,43 @@ main_cathw(int argc, char *argv[]) {
         return 0;
 }
 
-main_cat(int argc, char*argv[]) {
-	FILE *fp;
-
-        if (argc == 1) // No args; copy standard input
-                filecopy(stdin, stdout);
-        else
-                while (--argc > 0)
-                        if ((fp = fopen(*++argv, "r")) == NULL)
-                        {
-                                printf("cat: can't open %s\n", *argv);
-                                return 1;
-                        }
-                        else
-                        {
-                                filecopy(fp, stdout);
-                                fclose(fp);
-                        }
-        return 0;
-}
-
 int main(int argc, char **argv) {
-	uint64_t diff;
-	struct timespec start, end;
-	int i;
+	
+	/*cathw: system calls*/
+		uint64_t diff;
+		struct timespec start, end;
+		int i;
 
-  	// measure time
-	clock_gettime(CLOCK_MONOTONIC, &start);	// mark start time 
-	main_cathw(argc, argv);	// call cat routine
-	clock_gettime(CLOCK_MONOTONIC, &end);	// mark the end time 
+	  	// measure time
+		clock_gettime(CLOCK_MONOTONIC, &start);	// mark start time 
+		main_cathw(argc, argv);	// call cat routine
+		clock_gettime(CLOCK_MONOTONIC, &end);	// mark the end time 
+	
+  		// calcuate difference
+		diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+		printf("Run time of cathw = %llu nanoseconds\n", (long long unsigned int) diff);
+	
+	/*cat: normal printf/f functions*/
+		uint64_t diff2;
+        
+                // measure time
+                clock_gettime(CLOCK_MONOTONIC, &start); // mark start time 
+                main_cat(argc, argv); // call cat routine
+                clock_gettime(CLOCK_MONOTONIC, &end);   // mark the end time 
 
-  	// calcuate difference
-	diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
-	printf("Run time of cathw = %llu nanoseconds\n", (long long unsigned int) diff);
-
+                // calcuate difference
+                diff2 = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+                printf("Run time of cat = %llu nanoseconds\n", (long long unsigned int) diff2);
 	return 0;
 }
+
+void filecopy(FILE *ifp, FILE *ofp)
+{
+        int c;
+        while ((c = getc(ifp)) != EOF)
+                putc(c, ofp);
+}
+
 
 void filecopycathw(int ifd, int ofd) {
         int c;
